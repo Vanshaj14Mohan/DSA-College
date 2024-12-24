972b7d85b7e73e2bcf1efcec7cd39fab22fc52d2
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-12-24 23:45:11 +0530

    changes

--- Practical/ques_2.c
+++ Practical/ques_2.c
@@ -2,10 +2,8 @@
 #include<stdio.h>
 int main(){
     int num1=0,num2=1,num3,i,num;
-
     printf("Enter the Number till Fibonacci series need to print: ");
     scanf("%d",&num);
-
     printf("%d\t%d",num1,num2);
     num3= num1 + num2;
     for(i=2;i<num;i++)
